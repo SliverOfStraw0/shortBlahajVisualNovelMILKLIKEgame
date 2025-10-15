bc9f79e1be49dbf8f61d64c737af1f5d3de1e4ad
A-lox <simnmickal@gmail.com>
2025-10-15 19:16:48 -0400

    added some images to storyline and started on ending screen

--- globalS.gd
+++ globalS.gd
@@ -16,4 +16,7 @@ func chnageToMainSave(saveName):
 func chnageToExplodedEnding():
 	get_tree().change_scene_to_file("res://scenes/kaboom_ending.tscn")
 	
+func getsInTheCarEnding():
+	get_tree().change_scene_to_file("res://scenes/gets_in_the_car_ending.tscn")
+	
 var ver: String = "ALPHA 0.1.0"
